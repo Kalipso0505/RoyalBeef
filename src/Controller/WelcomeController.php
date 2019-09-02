3ce0c3b36b6bf7d80711d08ae7443f329e410686
Alexander Achim <a.achim@demv.de>
2019-09-02 23:10:52 +0200

    first page okay

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -13,7 +13,12 @@ class WelcomeController extends AbstractController
     public function index()
     {
         return $this->render('welcome/index.html.twig', [
-            'avatars' => ['kalipso', 'joni', 'chris', 'rens', 'kos', 'baerti'],
+            'avatars1' => ['kalipso', 'joni', 'chris', 'rens', 'kos', 'baerti'],
+            'avatars2' => ['joni', 'chris', 'rens', 'kos', 'baerti', 'kalipso'],
+            'avatars3' => ['chris', 'rens', 'kos', 'baerti', 'kalipso', 'joni'],
+            'avatars4' => ['rens', 'kos', 'baerti', 'kalipso', 'joni', 'chris'],
+            'avatars5' => ['kos', 'baerti', 'kalipso', 'joni', 'chris', 'rens'],
+            'avatars6' => ['baerti', 'kalipso', 'joni', 'chris', 'rens', 'kos'],
         ]);
     }
 }
