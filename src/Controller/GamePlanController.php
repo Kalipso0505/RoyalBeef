3bd474f0cc0b9d0ef58784fcc1f6e1a91cc2cce1
Alexander Achim <a.achim@demv.de>
2019-09-21 14:22:08 +0200

    second version of gamePlan

--- src/Controller/GamePlanController.php
+++ src/Controller/GamePlanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\BeefDataRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,18 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class GamePlanController extends AbstractController
 {
     /**
-     * @Route("/game/plan/{game}/{type}/", name="game_plan")
+     * @Route("/game/plan/{year}/{game}/{type}/", name="game_plan")
+     * @param int    $year
      * @param string $game
      * @param string $type
      *
      * @return Response
      */
-    public function index(string $game, string $type): Response
+    public function index(int $year, string $game, string $type): Response
     {
         $game = urldecode($game);
-        dd($game, $type);
+        $beefDataPath = $this->getParameter('kernel.root_dir') . '/../games/';
+        $gamer = BeefDataRepository::player($beefDataPath, $year);
+        dd($game, $type, $gamer);
         return $this->render('game_plan/index.html.twig', [
             'game' => $game,
+            'year' => $year
         ]);
     }
 }
